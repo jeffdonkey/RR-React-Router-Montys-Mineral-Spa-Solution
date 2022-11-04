c93b456f3d3f43e37898ed6ae837f214c8e738ee
Susan Heller <*jefa8@gmail.com>
2022-11-04 12:43:50 -0400

    done

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,27 @@
-
+import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/esm/Container'
+import Card from 'react-bootstrap/Card'
 
 export default function Packages(props) {
-    const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
+    const displayPackages = props.packages.map((eachPackage) => 
+        <ListGroup.Item>{eachPackage}</ListGroup.Item>)
 
     return (
         <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
+            <Container>
+               <Card>
+                <Card.Body>
+                    <Card.Title>Our Packages</Card.Title>
+                    <Card.Text>
+                        Check out some of our packages!  Every package is ethically sourced and organic!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup>
                     {displayPackages}
-                </ul>
-            </div>
+                </ListGroup>
+               </Card>
+
+            </Container>
         </div>
     )
 }
